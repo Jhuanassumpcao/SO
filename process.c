7c66afddc8ad6dad9a624cea14e43c88d1a5c965
jhuanassumpcao <jhuanassumpcao@gmail.com>
2023-04-25 20:59:09 -0300

    nova

--- process.c
+++ process.c
@@ -1,37 +1,40 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <time.h>
 
 #define MAX_DEPTH 4
 
 void create_processes(int current_depth, int max_depth, pid_t parent_pid) {
+    if (current_depth > max_depth) {
+        return;
+    }
 
-	if(current_depth > max_depth){
-		return;
-	}
-
-	pid_t pid;
-
-	if((pid = fork()) == 0){
-		printf("n=%d C[%d, %d] \n", current_depth, getpid(), parent_pid);
-		create_processes(current_depth + 1, max_depth, getpid());
-		printf("T[%d, %d]\n", getpid(), getppid());
-		exit(0);
-	}else {
-		waitpid(pid, NULL, 0);
-	}
-
+    pid_t pid;
+    if ((pid = fork()) == 0) { // child process
+        printf("n=%d C[%d, %d]\n", current_depth, getpid(), getppid());
+        create_processes(current_depth + 1, max_depth, getpid());
+        printf("T[%d, %d]\n", getpid(), getppid());
+        exit(0);
+    } else if (pid < 0) { // error
+        printf("Error creating process\n");
+        exit(1);
+    } else { // parent process
+        waitpid(pid, NULL, 0); // wait for child process to finish
+    }
 }
 
 int main() {
-	printf("Creating process tree...\n");
+    printf("Creating process tree...\n");
 
-	pid_t main_pid = getpid();
-	create_processes(1, MAX_DEPTH, main_pid);
+    clock_t start_time = clock();
+    pid_t main_pid = getpid();
+    create_processes(1, MAX_DEPTH, main_pid);
+    wait(NULL); // wait for all child processes to finish
+    clock_t end_time = clock();
 
-	printf("Process tree creating finish.\n");
-	
-	return 0;
+    printf("Process tree creation finished in %f seconds.\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+    return 0;
 }
