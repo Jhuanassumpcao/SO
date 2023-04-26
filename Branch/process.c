3b917da5f2d6c0cdc56e924f53b41e144d3940c6
jhuanassumpcao <jhuanassumpcao@gmail.com>
2023-04-25 21:33:41 -0300

    branch

--- Branch/process.c
+++ Branch/process.c
@@ -4,7 +4,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-#define TREE_HEIGHT 2
+#define TREE_HEIGHT 4
 
 void create_tree(int height, pid_t parent_pid) {
     if (height == 0) {
@@ -19,16 +19,15 @@ void create_tree(int height, pid_t parent_pid) {
         printf("T[%d, %d]\n", getpid(), parent_pid);
         exit(0);
     } else { // parent process
-        waitpid(left_pid, NULL, 0); // wait for left child process to finish
-    }
-
-    if ((right_pid = fork()) == 0) { // right child process
-        printf("n=%d C[%d, %d]\n", TREE_HEIGHT - height + 1, getpid(), parent_pid);
-        create_tree(height - 1, getpid());
-        printf("T[%d, %d]\n", getpid(), parent_pid);
-        exit(0);
-    } else { // parent process
-        waitpid(right_pid, NULL, 0); // wait for right child process to finish
+        if ((right_pid = fork()) == 0) { // right child process
+            printf("n=%d C[%d, %d]\n", TREE_HEIGHT - height + 1, getpid(), parent_pid);
+            create_tree(height - 1, getpid());
+            printf("T[%d, %d]\n", getpid(), parent_pid);
+            exit(0);
+        } else { // parent process
+            waitpid(left_pid, NULL, 0); // wait for left child process to finish
+            waitpid(right_pid, NULL, 0); // wait for right child process to finish
+        }
     }
 }
 
@@ -36,7 +35,7 @@ int main() {
     printf("Creating process tree...\n");
 
     pid_t main_pid = getpid();
-    printf("n=0 PID= %d (root)\n", main_pid);
+    printf("n=%d PID= %d (root) \n", main_pid);
     create_tree(TREE_HEIGHT, main_pid);
     printf("T[%d, %d]\n", main_pid, main_pid);
 
